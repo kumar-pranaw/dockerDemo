58b092c0610dc0d566fee9e9687643c0f0f08f2f
technossuspranav <pranav.k.verma@technossus.com>
2020-01-15 11:59:30 +0530

    ïnitial commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dockerdemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public ActionResult<string[]> Values()
+        {
+            string[] values = { "Value1", "Value2", "Value3" };
+            return values;
+        }
+    }
+}
